fa35662d09f26e725fe8b56cd4b5afd7e915891f
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-04-20 19:19:18 -0500

    update

--- Token.js
+++
@@ -1,8 +0,0 @@
-class Token {
-    constructor(type, value) {
-        this.type = type; // token type (e.g., INTEGER, PLUS, MINUS, EOF)
-        this.value = value; // token value (e.g., 1, '+', '-')
-    }
-}
-
-export default Token;
