9c4161101f43b045706ce8141b360266094d143a
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-04-22 02:49:30 -0500

    maybe good

--- interpreter.js
+++ interpreter.js
@@ -1,17 +1,12 @@
 class Interpreter {
     interpret(statement) {
         if (Array.isArray(statement)) {
-            const functionName = statement.shift();
+            const functionName = statement.shift().name;
+            const args = statement[0].arguments; // Extract arguments
             if (functionName === 'cat') {
-                return this.catFunction(statement);
-            } else if (functionName === 'repeat') {
-                return this.repeatFunction(statement);
-            } else if (functionName === 'reverse') {
-                return this.reverseFunction(statement);
+                return this.catFunction(args);
             } else if (functionName === 'multiply') {
-                return this.multiplyFunction(statement);
-            } else if (functionName === 'HelloWorld') {
-                return this.HelloWorldFunction();
+                return this.multiplyFunction(args);
             } else {
                 throw new Error(`Unknown function: ${functionName}`);
             }
@@ -27,31 +22,12 @@ class Interpreter {
         return params[0];
     }
 
-    repeatFunction(params) {
-        if (params.length !== 2) {
-            throw new Error('repeat function expects exactly two parameters');
-        }
-        const [str, times] = params;
-        return str.repeat(times);
-    }
-
-    reverseFunction(params) {
-        if (params.length !== 1) {
-            throw new Error('reverse function expects exactly one parameter');
-        }
-        return params[0].split('').reverse().join('');
-    }
-
     multiplyFunction(params) {
         if (params.length !== 2) {
             throw new Error('multiply function expects exactly two parameters');
         }
         const [a, b] = params;
         return a * b;
-    }
-
-    HelloWorldFunction() {
-        return 'Hello, World!';
     }
 }
 
