f03f9005eebd8963cd4a553bc9f261bdd9740271
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-04-22 03:10:56 -0500

    added lots of console.logs

--- parser.js
+++ parser.js
@@ -7,6 +7,7 @@ class Parser {
 
     eat(type) {
         if (this.currentToken.type === type) {
+            console.log(`Eating token of type: ${type}`);
             this.currentTokenIndex++;
             if (this.currentTokenIndex < this.tokens.length) {
                 this.currentToken = this.tokens[this.currentTokenIndex];
@@ -21,48 +22,57 @@ class Parser {
     parseStatement() {
         const token = this.currentToken;
     
-        // Log the current token type
-        console.log('Current token type:', token.type);
+        console.log('Current token:', token); // Log the current token
+        console.log('Current token type:', token.type); // Log the current token type
     
         if (token.type === 'IDENTIFIER') {
+            console.log('Parsing identifier');
             this.eat('IDENTIFIER');
             if (this.currentToken.type === 'LPAREN') {
+                console.log('LPAREN found');
                 this.eat('LPAREN');
                 const result = this.parse();
                 if (this.currentToken.type === 'RPAREN') { // Check for RPAREN after parsing arguments
+                    console.log('RPAREN found after arguments');
                     this.eat('RPAREN');
                     return result;
                 } else {
                     throw new Error(`Expected RPAREN, got ${this.currentToken.type}`);
                 }
             } else {
+                console.log('No LPAREN found');
                 return [token.value];
             }
         } else if (token.type === 'OPERATOR') {
+            console.log('Parsing operator');
             this.eat('OPERATOR');
             return [token.value];
         } else if (token.type === 'STRING') {
+            console.log('Parsing string');
             this.eat('STRING');
             return [token.value];
         } else if (token.type === 'LPAREN') {
+            console.log('LPAREN found, parsing subexpression');
             this.eat('LPAREN');
             const result = this.parse();
             if (this.currentToken.type === 'RPAREN') { // Check for RPAREN after parsing subexpression
+                console.log('RPAREN found after subexpression');
                 this.eat('RPAREN');
                 return result;
             } else {
                 throw new Error(`Expected RPAREN, got ${this.currentToken.type}`);
             }
         } else if (token.type === 'SEMICOLON') {
+            console.log('Parsing semicolon');
             this.eat('SEMICOLON');
             return [];
         } else {
             throw new Error(`Invalid statement: ${token.type}`);
         }
     }
-    
 
     parse() {
+        console.log('Parsing tokens...');
         const statements = [];
         while (this.currentToken !== null) {
             statements.push(this.parseStatement());
