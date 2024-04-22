8befa29494f814846b11ad9558db2e178e0abad5
LongShotPlaya <J.Nixon@eagles.oc.edu>
2024-04-22 02:45:43 -0500

    good

--- interpreter.test.js
+++ interpreter.test.js
@@ -4,9 +4,9 @@ describe('Interpreter', () => {
     test('Interpret input code', () => {
         const interpreter = new Interpreter();
         const ast = [
-            { type: 'FUNCTION_CALL', name: 'cat', arguments: ['"Hello"'] },
-            { type: 'FUNCTION_CALL', name: 'multiply', arguments: ['2', '4'] },
-        ];
+            { name: 'cat', arguments: ['"Hello"'] },
+            { name: 'multiply', arguments: ['2', '4'] },
+        ];        
         const result = interpreter.interpret(ast);
         expect(result).toEqual(['Hello', 8]);
     });
